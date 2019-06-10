bc7a77976a8bd9623b31a77d23b217d51b754290
Adam Kowalski <atargis@gmail.com>
2019-06-10 13:11:03 +0200

    Important changes to run android & iOS version

--- ios/NativebaseKitchenSink/AppDelegate.m
+++ ios/NativebaseKitchenSink/AppDelegate.m
@@ -15,8 +15,16 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   NSURL *jsCodeLocation;
-
+  
+#ifdef DEBUG
+  // DEV
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+#else
+  // PROD
+  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+#endif
+
+//  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"NativebaseKitchenSink"
